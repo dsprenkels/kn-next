f751279c41d119aaf962c2fa9b073743ec4cefd1
Daan Sprenkels <dsprenkels@gmail.com>
2013-04-28 02:06:21 +0200

    Removed unnecesary images

--- header_img.php
+++ header_img.php
@@ -1,10 +1,8 @@
 <?php
 
-//header("Cache-Control: must-revalidate");
-
 header("Cache-Control: max-age=3600, private");
 
-$img = "bente_u.jpg";
+$img = "img/bente_l.jpg";
 
 header("Status: 302");
 header("Location: $img");
