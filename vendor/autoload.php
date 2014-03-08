981a85f8bb3bb090f4cdc7be4331c5744ca3c6d7
Daan Sprenkels <dsprenkels@gmail.com>
2014-03-08 14:34:52 +0100

    twig update
    Signed-off-by: Daan Sprenkels <dsprenkels@gmail.com>

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit67255150e85148a3187912e31b1aa708::getLoader();
+return ComposerAutoloaderInitef70f910627c93946d21ec29fefea25d::getLoader();
